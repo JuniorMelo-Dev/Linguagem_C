b9dcf6e7ce24a9a4582878240e6acb219b0dbe7f
JuniorMelo <juniormelo.dev@gmail.com>
2022-03-25 10:24:13 -0300

    Commit inicial

---
+++ aula1.c
@@ -0,0 +1,9 @@
+#include <stdio.h> //biblioteca para usar os métodos de entrada e saída de dados, tipo = "printf"
+
+int main() {
+
+    printf("Aprendendo a linguagem C!\n");
+
+    return 0;
+
+}
---
+++ aula2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>//biblioteca para usar os métodos de entrada e saída de dados, tipo = "printf"
+
+int main() {
+
+    //variáveis => tipo nomeVariável = valorVariável
+    int valor = 0;
+
+    valor = 10;
+    printf("%d", valor);
+
+    
+
+    return 0;
+}
