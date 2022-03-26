1512a45b2f7de49071ee2fe377b45555a724b3c7
JuniorMelo <juniormelo.dev@gmail.com>
2022-03-26 13:26:28 -0300

    indentação

---
+++ Aulas_C/aula6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main() 
+{
+    /*  Bloco de Comentário
+
+        Instruções
+        Indentação
+        Comentário C 
+
+    */
+
+    // Comentário em Linha
+
+   printf("");
+
+   return 0;
+}
